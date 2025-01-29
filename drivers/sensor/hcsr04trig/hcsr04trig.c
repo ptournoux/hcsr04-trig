7c0e4f59fa016b2c960e1d73df6bd18d65c9ee36
Pierre Tournoux <tournoux@gmail.com>
2025-01-29 15:28:33 +0400

    v1 stable#tag v1

--- drivers/sensor/hcsr04trig/hcsr04trig.c
+++ drivers/sensor/hcsr04trig/hcsr04trig.c
@@ -11,7 +11,7 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(HC_SR04, CONFIG_SENSOR_LOG_LEVEL);
+LOG_MODULE_REGISTER(HCSR04TRIG, CONFIG_SENSOR_LOG_LEVEL);
 
 #define HC_SR04_MM_PER_MS     171
 
@@ -19,7 +19,7 @@ LOG_MODULE_REGISTER(HC_SR04, CONFIG_SENSOR_LOG_LEVEL);
 
 static const uint32_t hw_cycles_per_ms = sys_clock_hw_cycles_per_sec() / 1000;
 
-struct hcsr04_data {
+struct hcsr04trig_data {
 	const struct device *dev;
 	struct gpio_callback gpio_cb;
 	struct k_sem sem;
@@ -27,14 +27,13 @@ struct hcsr04_data {
 	atomic_t echo_high_cycles;
 };
 
-struct hcsr04_config {
+struct hcsr04trig_config {
 	struct gpio_dt_spec trigger_gpios;
-	struct gpio_dt_spec echo_gpios;
 };
 
-static void hcsr04_gpio_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
+static void hcsr04trig_gpio_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
 
-static int hcsr04_configure_gpios(const struct hcsr04_config *cfg)
+static int hcsr04trig_configure_gpios(const struct hcsr04trig_config *cfg)
 {
 	int ret;
 
@@ -48,23 +47,10 @@ static int hcsr04_configure_gpios(const struct hcsr04_config *cfg)
 			ret);
 		return ret;
 	}
-	// If two pins model, configure echo pin
-	if(GROVE_ULTRASONIC_SENSOR == 0){
-		if (!gpio_is_ready_dt(&cfg->trigger_gpios)) {
-			LOG_ERR("GPIO '%s' not ready", cfg->trigger_gpios.port->name);
-			return -ENODEV;
-		}
-		ret = gpio_pin_configure_dt(&cfg->trigger_gpios, GPIO_INPUT);
-		if (ret < 0) {
-			LOG_ERR("Failed to configure '%s' as output: %d", cfg->trigger_gpios.port->name, ret);
-			return ret;
-		}
-	}
-
 	return 0;
 }
 
-static int hcsr04_configure_interrupt(const struct hcsr04_config *cfg, struct hcsr04_data *data)
+static int hcsr04trig_configure_interrupt(const struct hcsr04trig_config *cfg, struct hcsr04trig_data *data)
 {
 	int ret;
 
@@ -76,7 +62,7 @@ static int hcsr04_configure_interrupt(const struct hcsr04_config *cfg, struct hc
 			ret);
 		return -EIO;
 	}
-	gpio_init_callback(&data->gpio_cb, &hcsr04_gpio_callback, BIT(cfg->trigger_gpios.pin));
+	gpio_init_callback(&data->gpio_cb, &hcsr04trig_gpio_callback, BIT(cfg->trigger_gpios.pin));
 	ret = gpio_add_callback(cfg->trigger_gpios.port, &data->gpio_cb);
 	if (ret < 0) {
 		LOG_ERR("Failed to add callback on '%s': %d", cfg->trigger_gpios.port->name, ret);
@@ -85,34 +71,23 @@ static int hcsr04_configure_interrupt(const struct hcsr04_config *cfg, struct hc
 	return 0;
 }
 
-static int hcsr04_init(const struct device *dev)
+static int hcsr04trig_init(const struct device *dev)
 {
-	const struct hcsr04_config *cfg = dev->config;
-	struct hcsr04_data *data = dev->data;
+	const struct hcsr04trig_config *cfg = dev->config;
+	struct hcsr04trig_data *data = dev->data;
 	int ret;
-	LOG_ERR("hcsr04_init trigger pin/port %d/%d echo pin %d/%d",cfg->trigger_gpios.pin,cfg->trigger_gpios.port,cfg->trigger_gpios.pin,cfg->trigger_gpios.port);
 	k_sem_init(&data->sem, 0, 1);
-
-	ret = hcsr04_configure_gpios(cfg);
+	ret = hcsr04trig_configure_gpios(cfg);
 	if (ret < 0) {
 		return ret;
 	}
-	// If two pins model, configure interrupt at the beggining
-	if(GROVE_ULTRASONIC_SENSOR == 0){
-		ret = hcsr04_configure_interrupt(cfg, data);
-		if (ret < 0) {
-			return ret;
-		}
-	}
-	
-
 	return 0;
 }
 
-static void hcsr04_gpio_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+static void hcsr04trig_gpio_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-	struct hcsr04_data *data = CONTAINER_OF(cb, struct hcsr04_data, gpio_cb);
-	const struct hcsr04_config *cfg = data->dev->config;
+	struct hcsr04trig_data *data = CONTAINER_OF(cb, struct hcsr04trig_data, gpio_cb);
+	const struct hcsr04trig_config *cfg = data->dev->config;
 	//LOG_ERR("CB ");
 	if (gpio_pin_get(dev, cfg->trigger_gpios.pin) == 1) {
 		data->start_cycles = k_cycle_get_32();
@@ -123,22 +98,17 @@ static void hcsr04_gpio_callback(const struct device *dev, struct gpio_callback 
 	}
 }
 
-static int hcsr04_sample_fetch(const struct device *dev, enum sensor_channel chan)
+static int hcsr04trig_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
-	const struct hcsr04_config *cfg = dev->config;
-	struct hcsr04_data *data = dev->data;
+	const struct hcsr04trig_config *cfg = dev->config;
+	struct hcsr04trig_data *data = dev->data;
 	int ret;
-	if (!gpio_is_ready_dt(&cfg->trigger_gpios)) {
-		LOG_ERR("GPIO '%s' not ready", cfg->trigger_gpios.port->name);
-		return -ENODEV;
-	}
 	ret = gpio_pin_configure_dt(&cfg->trigger_gpios, GPIO_OUTPUT_LOW);
 	if (ret < 0) {
 		LOG_ERR("Failed to configure '%s' as output: %d", cfg->trigger_gpios.port->name,
 			ret);
 		return ret;
 	}
-
 	
 
 	/* Generate 10us trigger */
@@ -155,44 +125,34 @@ static int hcsr04_sample_fetch(const struct device *dev, enum sensor_channel cha
 		LOG_ERR("Failed to set trigger pin: %d", ret);
 		return ret;
 	}
+	// Now change PIN config to input
 	ret = gpio_pin_configure_dt(&cfg->trigger_gpios, GPIO_INPUT);
-	LOG_ERR("gpio_pin_configure_dt set trigger pin as input: %d", ret);
-	ret = gpio_pin_interrupt_configure_dt(&cfg->trigger_gpios, GPIO_INT_EDGE_BOTH);	
-	LOG_ERR("gpio_pin_interrupt_configure_dt set trigger pin as input: %d", ret);
 	if (ret < 0) {
-		LOG_ERR("Failed to set configure echo pin as interrupt: %d", ret);
+		LOG_ERR("Failed to set configure pin as input: %d", ret);
 		return ret;
 	}
-	// Now GROVE_ULTRASONIC_SENSOR
-	if(GROVE_ULTRASONIC_SENSOR){
-		// Replace GPIO .. probably not safe
-		//cfg->trigger_gpios = cfg->trigger_gpios;
-		//ret = gpio_pin_configure_dt(&cfg->trigger_gpios, GPIO_OUTPUT_LOW);
-		if (ret < 0) {
-			LOG_ERR("Failed to configure '%s' as output: %d", cfg->trigger_gpios.port->name,
-			ret);
-			return ret;
-		}
-		ret = hcsr04_configure_interrupt(cfg, data);
-		ret = gpio_pin_interrupt_configure_dt(&cfg->trigger_gpios, GPIO_INT_EDGE_BOTH);
-		LOG_ERR("gpio_pin_interrupt_configure_dt set trigger pin as input: %d", ret);
-		if (ret < 0) {
-			LOG_ERR("Failed to set configure echo pin as interrupt: %d", ret);
-			return ret;
-		}
+	ret = hcsr04trig_configure_interrupt(cfg, data);
+	if (ret < 0) {
+		LOG_ERR("Failed to set configure pin as interrupt: %d", ret);
+		return ret;
 	}
-
-	if (k_sem_take(&data->sem, K_MSEC(100)) != 0) {
+	ret = gpio_pin_interrupt_configure_dt(&cfg->trigger_gpios, GPIO_INT_EDGE_BOTH);
+	if (ret < 0) {
+		LOG_ERR("Failed to set configure echo pin as interrupt w rising and falling edges: %d", ret);
+		return ret;
+	}
+	
+	if (k_sem_take(&data->sem, K_MSEC(10)) != 0) {
 		LOG_ERR("Echo signal was not received");
 		return -EIO;
 	}
 	return 0;
 }
 
-static int hcsr04_channel_get(const struct device *dev, enum sensor_channel chan,
+static int hcsr04trig_channel_get(const struct device *dev, enum sensor_channel chan,
 	struct sensor_value *val)
 {
-	const struct hcsr04_data *data = dev->data;
+	const struct hcsr04trig_data *data = dev->data;
 	uint32_t distance_mm;
 
 	if (chan != SENSOR_CHAN_DISTANCE) {
@@ -201,28 +161,27 @@ static int hcsr04_channel_get(const struct device *dev, enum sensor_channel chan
 
 	distance_mm = HC_SR04_MM_PER_MS * atomic_get(&data->echo_high_cycles) /
 			hw_cycles_per_ms;
-	LOG_ERR("Distance %d %d %d", distance_mm,val->val1, val->val2);
 	return sensor_value_from_milli(val, distance_mm);
 }
 
 static DEVICE_API(sensor, hcsr04trig_driver_api) = {
-	.sample_fetch = hcsr04_sample_fetch,
-	.channel_get = hcsr04_channel_get
+	.sample_fetch = hcsr04trig_sample_fetch,
+	.channel_get = hcsr04trig_channel_get
 };
 
 
 #define HCSR04TRIG_INIT(index)                                                           \
-	static struct hcsr04_data hcsr04_data_##index = {                             \
+	static struct hcsr04trig_data hcsr04trig_data_##index = {                             \
 		.dev = DEVICE_DT_INST_GET(index),                                     \
 		.start_cycles = 0,                                                    \
 		.echo_high_cycles = ATOMIC_INIT(0),                                   \
 	};                                                                            \
-	static struct hcsr04_config hcsr04_config_##index = {                         \
+	static struct hcsr04trig_config hcsr04trig_config_##index = {                         \
 		.trigger_gpios = GPIO_DT_SPEC_INST_GET(index, trigger_gpios),         \
 	};                                                                            \
                                                                                       \
-	SENSOR_DEVICE_DT_INST_DEFINE(index, &hcsr04_init, NULL, &hcsr04_data_##index, \
-				&hcsr04_config_##index, POST_KERNEL,                  \
-				CONFIG_SENSOR_INIT_PRIORITY, &hcsr04_driver_api);     \
+	SENSOR_DEVICE_DT_INST_DEFINE(index, &hcsr04trig_init, NULL, &hcsr04trig_data_##index, \
+				&hcsr04trig_config_##index, POST_KERNEL,                  \
+				CONFIG_SENSOR_INIT_PRIORITY, &hcsr04trig_driver_api);     \
 
-DT_INST_FOREACH_STATUS_OKAY(HC_SR04_INIT)
+DT_INST_FOREACH_STATUS_OKAY(HCSR04TRIG_INIT)
